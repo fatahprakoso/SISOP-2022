1e7cf4dd932ec0319982b7148a404dbc149f0564
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-04-21 20:34:29 +0700

    rename folder

---
+++ 2.Shell_Script/soal-e.sh
@@ -0,0 +1,8 @@
+read input
+
+while [[ $input -ge 0 ]]; do
+	echo $input
+	read input
+done
+
+echo $input
---
+++ 2.Shell_Script/soal-g.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $RANDOM
+echo $RANDOM
+echo $RANDOM
+echo $RANDOM
+echo $(($RANDOM*-1))
+
